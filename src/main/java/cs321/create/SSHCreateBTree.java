45e5af3afc89935563f1b7efbfb4728c2078e1de
codyt-bsu <codytoone@u.boisestate.edu>
2025-12-13 01:48:57 +0000

    proper option reading for sshbtree

--- src/main/java/cs321/create/SSHCreateBTree.java
+++ src/main/java/cs321/create/SSHCreateBTree.java
@@ -19,11 +19,18 @@ public class SSHCreateBTree {
      */
     public static void main(String[] args) throws Exception 
 	{
+        if (args.length == 0){
+            System.out.println("java -jar build/libs/SSHCreateBTree.jar --cache=<0/1> --degree=<btree-degree> \\\r\n" + //
+                                "          --sshFile=<ssh-File> --type=<tree-type> [--cache-size=<n>] \\\r\n" + //
+                                "          --database=<yes/no> [--debug=<0|1>]");
+            return;
+        }
 		// System.out.println("Hello world from cs321.create.SSHCreateBTree.main");
         SSHCreateBTreeArguments myArgs = parseArguments(args);
         // other code 
         BTree btree = new BTree(myArgs.getDegree(), myArgs.getSSHFileName());
         btree.dumpToDatabase("jdbc:sqlite:SSHLogDB.db", "SSHLog");
+        // btree.dumpToFile();
         btree.close();
         
 	}
@@ -35,7 +42,44 @@ public class SSHCreateBTree {
      */
     public static SSHCreateBTreeArguments parseArguments(String[] args) throws ParseArgumentException
     {
-        SSHCreateBTreeArguments bta = new SSHCreateBTreeArguments(Boolean.parseBoolean(args[0]), Integer.parseInt(args[1]), args[2], args[3], Integer.parseInt(args[4]), Integer.parseInt(args[4]));
+        int degree;
+        try{
+            degree = Integer.parseInt(getOpt("--degree=", args));
+        }catch(Exception e){
+            // System.err.println(e.getMessage());
+            degree = 0;
+        }
+
+        boolean cache;
+        if (getOpt("--cache=", args).equals("1")){
+            cache = true;
+        }else {
+            cache = false;
+        }
+
+        int cacheSize;
+        try{
+            cacheSize = Integer.parseInt(getOpt("--cache-size=", args));
+        }catch(Exception e){
+            // System.err.println(e.getMessage());
+            cacheSize = 0;
+        }
+
+        int debugLevel;
+        try{
+            debugLevel = Integer.parseInt(getOpt("--debug=", args));
+        }catch(Exception e){
+            // System.err.println(e.getMessage());
+            debugLevel = 0;
+        }
+        
+        SSHCreateBTreeArguments bta = new SSHCreateBTreeArguments(
+                cache, 
+                degree, 
+                getOpt("--sshFile=", args), 
+                getOpt("--type=", args), 
+                cacheSize, 
+                debugLevel);
 
         return bta;
     }
@@ -50,5 +94,21 @@ public class SSHCreateBTree {
         System.err.println(errorMessage);
         System.exit(1);
 	}
+
+
+    /**
+     * Returns a string value for a command line option (usually beginning with --)
+     * @param optName the option name (include -- and =)
+     * @param args command line arguments
+     * @return first option string minus nameString
+     */
+    private static String getOpt(String optName, String[] args){
+        for(String s : args){
+            if (s.startsWith(optName)){
+                return s.substring(optName.length());
+            }
+        }
+        return null;
+    }
 
 }
